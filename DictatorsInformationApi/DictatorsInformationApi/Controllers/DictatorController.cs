9a8ad72c5c9a223036e0793da976aab540c32c2d
Steffeb Halberg <steffen.halberg@gmail.com>
2022-01-26 13:00:54 +0100

    Made basic logic for Api

---
+++ DictatorsInformationApi/DictatorsInformationApi/Controllers/DictatorController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace DictatorsInformationApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DictatorController : Controller
+    {
+        private List<DictatorModel> dictators = new List<DictatorModel>();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<DictatorModel>> GetDictators()
+        {
+            dictators.Add(new DictatorModel("Steffen", "yy", "1985-08-03" , "10-10-2090", "this is a test"));
+            return Ok( dictators);
+        }
+
+        [HttpPost]
+        public ActionResult<DictatorModel> PostDictator(DictatorModel dictator)
+        {
+            var model = dictator;
+
+            dictators.Add(dictator);
+            return Ok( dictator);
+        }
+    }
+}
---
+++ DictatorsInformationApi/DictatorsInformationApi/DictatorModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictatorsInformationApi
+{
+    public class DictatorModel
+    {
+        #region fields
+        private string _firstName;
+        private string _lastName;
+        private string _birth;
+        private string _death;
+        private string _description;
+        #endregion
+
+        #region Constructor
+
+        public DictatorModel()
+        {
+
+        }
+        public DictatorModel(string firstName, string lastname, string birth, string death, string description)
+        {
+            FirstName = firstName;
+            LastName = lastname;
+            Birth = birth;
+            Death = death;
+            Description = description;
+        }
+        #endregion
+
+        #region Properties
+        [Required]
+        [MaxLength(80)]
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+        [Required]
+        [MaxLength(80)]
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+        public string Birth
+        {
+            get { return _birth; }
+            set { _birth = value; }
+        }
+        public string Death
+        {
+            get { return _death; }
+            set { _death = value; }
+        }
+        [Required]
+        [MaxLength(80)]
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+        #endregion
+
+
+
+
+    }
+}
